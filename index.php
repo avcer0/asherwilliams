2dd9ab4b47aca167e1a6da07e81e61f9e463ae19
avcer0 <86516498+avcer0@users.noreply.github.com>
2025-03-22 22:44:02 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="generator" content="RocketCake">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index_php.css?h=5a81983b">
+</head>
+<body>
+<div class="textstyle1">
+<div id="container_4146eb3e"><div id="container_4146eb3e_padding" ><div class="textstyle1"><h1 id="heading_7d1752af">asher williams</h1><label id="label_18a61349">artist.creator.musician.drums.guitar.melody.blah</label>  <div id="text_428514aa">
+    <div class="textstyle1">
+      <span class="textstyle2"><a href="https://oblivionfly.bandcamp.com/">music me</a></span>
+      <div id="text_3c89489e">
+        <div class="textstyle1">
+          <span class="textstyle2"><a href="mailto:asherkwilliams07@gmail.com">email me</a></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  <img src="rc_images/sandhoppers1_1400x1050.jpg" width="1400" height="1050" id="img_274cbdb6" alt="" title="" />
+</div>
+<div style="clear:both"></div></div></div><div id="container_5ed68884"><div id="container_5ed68884_padding" ><div class="textstyle3">  <h1 id="heading_1f421e47">
+    <span class="textstyle4">
+      <span class="textstyle5">New demo album 'Tragectory Wave Gearbox Truancy Belief Inside' </span>
+      <span class="textstyle5"><a href="https://oblivionfly.bandcamp.com">out now</a></span>
+      </span>
+    </h1>
+</div>
+<div style="clear:both"></div></div></div><div id="container_2c7116ea"><div id="container_2c7116ea_padding" ><div class="textstyle1"><div id="container_2823f0b8"><div id="container_2823f0b8_padding" ><div class="textstyle1">  <div id="text_36e1e858">
+    <div class="textstyle1">
+      <span class="textstyle6">..22 march 2025</span>
+      <div id="text_341793ed">
+        <div class="textstyle1">
+          <span class="textstyle7">Welcome to my new blog site. This is where I'll be doing most of what I already do on Instagram.<br/><br/>I'm growing sick and tired of the generalization of social media and seeing how it's affecting the world. It's sad and regrettable to see so many people every day wasting hours on social media, especially out and about in the real world.<br/><br/>Anywho this is a new avenue I'm trying to pursue in hopes of living a more decentralized digital life, even if it's a small effort such as creating this site. I wouldn't go full steam ahead with it, ditching gmail and working my ass off to host my own services. Too broke.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<div style="clear:both"></div></div></div><div id="container_53d841d1"><div id="container_53d841d1_padding" ><div class="textstyle1"><div id="text_1fc3bde7"><div class="textstyle1">  <span class="textstyle8">practice log</span>
+</div>
+</div><div id="container_105956d6"><div id="container_105956d6_padding" ><div class="textstyle1">  <div id="text_38d296dd">
+    <div class="textstyle1">
+      <span class="textstyle9">..come back later</span>
+      </div>
+    </div>
+</div>
+<div style="clear:both"></div></div></div></div>
+<div style="clear:both"></div></div></div></div>
+<div style="clear:both"></div></div></div>  </div>
+</body>
+</html>
